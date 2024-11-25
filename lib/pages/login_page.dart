b99e3f1feb67dfe959baa337f3d3e527d41a71d0
DabangKim <nzkiwiking@gmail.com>
2024-11-25 17:17:20 +0900

    로그인 첫 커밋

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,62 +1,208 @@
-import 'package:appy_app/pages/add_module_page.dart';
+import 'package:flutter/material.dart';
 import 'package:appy_app/widgets/widget.dart';
 import 'package:appy_app/widgets/theme.dart';
-import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({
-    super.key,
-  });
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: BuildAppBar(context),
-        body: SafeArea(
-            child: Padding(
-                padding: AppPadding.body,
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
+      resizeToAvoidBottomInset: false, // 키보드가 나타날 때 화면 크기 조절 방지
+      appBar: BuildAppBar(context),
+      body: SafeArea(
+        child: GestureDetector( // 화면 터치 시 키보드 숨기기
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: Padding(
+            padding: AppPadding.body,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "로그인",
-                            style: TextStyle(
-                              fontSize: 40,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                          Container(
-                            width: 5,
-                          ),
-                        ],
+                      const Text(
+                        "로그인",
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w700,
+                        ),
                       ),
                       Container(
-                        height: 300,
+                        width: 5,
                       ),
-                      // 이메일 입력
-                      //
-                      // 비밀번호 입력
-                      //
-                      GestureDetector(
+                    ],
+                  ),
+                  Container(
+                    height: 300,
+                  ),
+                  // 이메일 입력
+                  TextField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: const Color.fromRGBO(127, 212, 173, 0.16),
+                      hintText: '이메일을 입력하세요',
+                      hintStyle: const TextStyle(color: Colors.black),
+                      contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: AppColors.accent),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: AppColors.accent),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                    onChanged: (text) {
+                      // 이메일 입력 필드 값 변경 시 버튼 상태 업데이트
+                      setState(() {});
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  // 비밀번호 입력
+                  TextField(
+                    controller: _passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: const Color.fromRGBO(127, 212, 173, 0.16),
+                      hintText: '비밀번호를 입력하세요',
+                      hintStyle: const TextStyle(color: Colors.black),
+                      contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: AppColors.accent),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: AppColors.accent),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                    onChanged: (text) {
+                      // 비밀번호 입력 필드 값 변경 시 버튼 상태 업데이트
+                      setState(() {});
+                    },
+                  ),
+                  const SizedBox(height: 24),
+                  // 로그인 버튼
+                  ElevatedButton(
+                    onPressed: _emailController.text.isNotEmpty && _passwordController.text.isNotEmpty
+                        ? () {
+                      // 로그인 성공 시 페이지 이동
+                    }
+                        : null, // 이메일 또는 비밀번호 입력 필드가 비어 있으면 버튼 비활성화
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      fixedSize: const Size(380, 50),
+                      foregroundColor: AppColors.textWhite,
+                      backgroundColor: (_emailController.text.isNotEmpty && _passwordController.text.isNotEmpty)
+                          ? AppColors.accent
+                          : AppColors.buttonDisabled,
+                    ),
+                    child: const Text("로그인"),
+                  ),
+                  const SizedBox(height: 24),
+                  // 계정이 없으신가요?
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "계정이 없으신가요? ",
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 14,
+                        ),
+                      ),
+                      InkWell(
                         onTap: () {
-                          // 로그인 처리 api
-                          //
-
-                          // 페이지 이동
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const AddModulePage()));
+                          // 회원가입 페이지로 이동
                         },
-                        // 모두 입력했을 때만 로그인 버튼 활성화하기
-                        //
-                        child: BuildButton(
-                            "로그인", AppColors.accent, AppColors.textWhite),
-                      ),
-                    ]))));
+                        child: const Text(
+                          "회원가입",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 14,
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  // 오류 팝업
+  void _showErrorDialog(BuildContext context, String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          backgroundColor: Colors.white,
+          title: Text(
+            message,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+            ),
+          ),
+          titlePadding: const EdgeInsets.only(top: 30),
+          contentPadding: const EdgeInsets.only(top: 10, bottom: 30),
+          actionsAlignment: MainAxisAlignment.center,
+          actions: [
+            SizedBox(
+              width: 130,
+              height: 40,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: AppColors.accent,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  textStyle: const TextStyle(
+                    fontSize: TextSize.small,
+                    fontFamily: "SUITE",
+                  ),
+                ),
+                child: const Text("확인"),
+              ),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -1,62 +1,251 @@
-import 'package:appy_app/pages/add_module_page.dart';
-import 'package:appy_app/widgets/widget.dart';
-import 'package:appy_app/widgets/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:appy_app/widgets/theme.dart';
 
-class SignUpPage extends StatelessWidget {
-  const SignUpPage({
-    super.key,
-  });
+class SignupPage extends StatefulWidget {
+  const SignupPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _nameController = TextEditingController();
+  final _ageController = TextEditingController();
+  bool _isMale = true;
+  bool _isEmailChecked = false;
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    _nameController.dispose();
+    _ageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: BuildAppBar(context),
-        body: SafeArea(
-            child: Padding(
-                padding: AppPadding.body,
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "회원가입",
-                            style: TextStyle(
-                              fontSize: 40,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                          Container(
-                            width: 5,
-                          ),
-                        ],
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 20),
+                  const Text(
+                    "회원가입",
+                    style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  const SizedBox(height: 50),
+                  // 이메일 입력
+                  TextField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: const Color.fromRGBO(127, 212, 173, 0.16),
+                      hintText: '이메일을 입력하세요',
+                      hintStyle: const TextStyle(color: Colors.black),
+                      contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: AppColors.accent),
+                        borderRadius: BorderRadius.circular(8.0),
                       ),
-                      Container(
-                        height: 300,
-                      ),
-                      // 이메일 입력
-                      //
-                      // 비밀번호 입력
-                      //
-                      GestureDetector(
-                        onTap: () {
-                          // 로그인 처리 api
-                          //
-
-                          // 페이지 이동
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const AddModulePage()));
+                      suffixIcon: _isEmailChecked
+                          ? null
+                          : TextButton(
+                        onPressed: () {
+                          // 중복 확인 API 호출
+                          setState(() {
+                            _isEmailChecked = true; // 중복 확인 완료 상태로 변경
+                          });
                         },
-                        // 모두 입력했을 때만 회원가입 버튼 활성화하기
-                        //
-                        child: BuildButton(
-                            "회원가입", AppColors.accent, AppColors.textWhite),
+                        child: const Text(
+                          "중복 확인",
+                          style: TextStyle(
+                            color: AppColors.accent,
+                            fontSize: 14,
+                          ),
+                        ),
                       ),
-                    ]))));
+                    ),
+                    onChanged: (text) {
+                      // 이메일 입력 필드 값 변경 시 버튼 상태 업데이트
+                      setState(() {
+                        _isEmailChecked = false; // 이메일 변경 시 중복 확인 초기화
+                      });
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  // 비밀번호 입력
+                  TextField(
+                    controller: _passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: const Color.fromRGBO(127, 212, 173, 0.16),
+                      hintText: '비밀번호를 입력하세요',
+                      hintStyle: const TextStyle(color: Colors.black),
+                      contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: AppColors.accent),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                    onChanged: (text) {
+                      // 비밀번호 입력 필드 값 변경 시 버튼 상태 업데이트
+                      setState(() {});
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  // 이름 입력
+                  TextField(
+                    controller: _nameController,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: const Color.fromRGBO(127, 212, 173, 0.16),
+                      hintText: '이름을 입력하세요',
+                      hintStyle: const TextStyle(color: Colors.black),
+                      contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: AppColors.accent),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                    onChanged: (text) {
+                      // 이름 입력 필드 값 변경 시 버튼 상태 업데이트
+                      setState(() {});
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  // 나이 입력
+                  TextField(
+                    controller: _ageController,
+                    keyboardType: TextInputType.number, // 숫자 키보드
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: const Color.fromRGBO(127, 212, 173, 0.16),
+                      hintText: '나이를 입력하세요',
+                      hintStyle: const TextStyle(color: Colors.black),
+                      contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: AppColors.accent),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                    onChanged: (text) {
+                      // 나이 입력 필드 값 변경 시 버튼 상태 업데이트
+                      setState(() {});
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  // 성별 선택
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      _genderToggleButton("남", _isMale),
+                      _genderToggleButton("여", !_isMale),
+                    ],
+                  ),
+                  const SizedBox(height: 30),
+                  // 회원가입 버튼
+                  ElevatedButton(
+                    onPressed: _emailController.text.isNotEmpty &&
+                    _passwordController.text.isNotEmpty &&
+                    _nameController.text.isNotEmpty &&
+                    _ageController.text.isNotEmpty &&
+                    _isEmailChecked ? () {
+                      // 회원가입 처리
+                    }
+                        : null,
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      fixedSize: const Size(380, 50),
+                      foregroundColor: AppColors.textWhite,
+                      backgroundColor: (_emailController.text.isNotEmpty &&
+                          _passwordController.text.isNotEmpty &&
+                          _nameController.text.isNotEmpty &&
+                          _ageController.text.isNotEmpty &&
+                          _isEmailChecked)
+                          ? AppColors.accent
+                          : AppColors.buttonDisabled,
+                      ),
+                      child: const Text("회원가입"),
+                  ),
+                  const SizedBox(height: 24),
+                  // 계정이 있으신가요?
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "계정이 있으신가요? ",
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 14,
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                        // 로그인 페이지로 이동
+                        },
+                        child: const Text(
+                          "로그인",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 14,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 50), // 아래 여백 추가
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  // 성별 토글 버튼
+  Widget _genderToggleButton(String gender, bool isSelected) {
+    return SizedBox(
+      width: 130,
+      height: 40,
+      child: ElevatedButton(
+        onPressed: () {
+          setState(() {
+            _isMale = (gender == "남");
+          });
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: isSelected ? AppColors.accent : AppColors.buttonDisabled,
+          foregroundColor: AppColors.textWhite,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+          textStyle: const TextStyle(
+            fontSize: TextSize.small,
+            fontFamily: "SUITE",
+          ),
+        ),
+        child: Text(gender),
+      ),
+    );
   }
 }
