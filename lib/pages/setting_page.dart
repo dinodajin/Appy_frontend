71647fa3d9538c37f6f66fc2d2211743509d086f
kyogi0321 <kiok321@naver.com>
2024-11-26 18:17:36 +0900

    1125 1817 commit

--- lib/pages/setting_page.dart
+++ lib/pages/setting_page.dart
@@ -13,13 +13,38 @@ class SettingPage extends StatelessWidget {
     return Scaffold(
       appBar: BuildSettingAppBar(context, "사용자 설정"),
       body: SafeArea(
-          child: Padding(
-              padding: AppPadding.body,
-              child: Column(children: [
-                Container(
-                  height: 20,
-                ),
-                Row(
+        child: Padding(
+          padding: AppPadding.body,
+          child: Column(
+            children: [
+              Container(
+                height: 20,
+              ),
+              GestureDetector(
+                onTap: () {
+                  // "기기 등록" 클릭 시 팝업 호출
+                  showCustomErrorDialog(
+                    context: context,
+                    message: "기기를 등록해야 사용할 수 있습니다.",
+                    buttonText: "확인",
+                    onConfirm: () {
+                      // 확인 버튼 동작
+                      Navigator.of(context).pop();
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text("확인 버튼을 눌렀습니다.")),
+                      );
+                    },
+                    cancelButtonText: "취소",
+                    onCancel: () {
+                      // 취소 버튼 동작
+                      Navigator.of(context).pop();
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text("취소 버튼을 눌렀습니다.")),
+                      );
+                    },
+                  );
+                },
+                child: Row(
                   children: [
                     const Icon(
                       Icons.add_home_outlined,
@@ -39,14 +64,18 @@ class SettingPage extends StatelessWidget {
                     ),
                   ],
                 ),
-                Container(
-                  height: 10,
-                ),
-                Container(
-                  color: AppColors.buttonDisabled,
-                  height: 1,
-                ),
-              ]))),
+              ),
+              Container(
+                height: 10,
+              ),
+              Container(
+                color: AppColors.buttonDisabled,
+                height: 1,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
